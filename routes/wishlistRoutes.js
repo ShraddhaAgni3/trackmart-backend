290d88249e44d005b5c7f8ec2115e5d6217a04f5
ShraddhaAgni3 <155717879+ShraddhaAgni3@users.noreply.github.com>
2026-03-10 16:01:24 +0530

    Add files via upload

---
+++ routes/wishlistRoutes.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+  addToWishlist,
+  getWishlist,
+  removeFromWishlist
+} from "../controllers/wishlistController.js";
+
+import { protect } from "../middleware/auth.js";
+
+const router = express.Router();
+
+router.get("/", protect, getWishlist);
+
+router.post("/", protect, addToWishlist);
+
+router.delete("/:id", protect, removeFromWishlist);
+
+export default router;
