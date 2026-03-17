aebe62646dc4e282b21fe1e344364a3dffda65fd
ShraddhaAgni3 <155717879+ShraddhaAgni3@users.noreply.github.com>
2026-03-17 12:34:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+import pool from "./config/db.js";
 import app from "./app.js";
+
+pool.connect()
+.then(() => console.log("Database connected"))
+.catch(err => console.log("DB error:", err));
 
 const PORT = process.env.PORT || 5000;
 
