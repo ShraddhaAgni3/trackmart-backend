c90aa09f2b99c17e232849a915220e436fe096a0
ShraddhaAgni3 <155717879+ShraddhaAgni3@users.noreply.github.com>
2026-03-23 16:51:40 +0530

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -199,7 +199,7 @@ export const createProduct = async (req, res) => {
       how_to_use,making_process,
       image_url,ingredients_image_url)
       VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,
-      $11,$12,$13,$14,$15,$16,$17,$18,$19,$20)
+      $11,$12,$13,$14,$15,$16,$17,$18,$19)
       RETURNING *
       `,
       [
