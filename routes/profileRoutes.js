1ce8266af1cf07618955316a0290eb32d1c0723f
ShraddhaAgni3 <155717879+ShraddhaAgni3@users.noreply.github.com>
2026-03-28 13:38:01 +0530

    Update profileRoutes.js

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { getProfile, updateProfile } from "../controllers/profileController.js";
-import { register, login } from "../controllers/authController.js";
+import { protect } from "../middleware/auth.js"; // ✅ use same middleware
 
 const router = express.Router();
 
-router.get("/profile", authMiddleware, getProfile);
-router.put("/profile", authMiddleware, updateProfile);
+router.get("/profile", protect, getProfile);
+router.put("/profile", protect, updateProfile);
 
 export default router;
