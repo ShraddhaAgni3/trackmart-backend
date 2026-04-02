24354973e822eb1d257616bf10a11755f9049cee
ShraddhaAgni3 <155717879+ShraddhaAgni3@users.noreply.github.com>
2026-04-02 19:40:48 +0530

    Update orderRoutes.js

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createOrder,getUserOrders } from "../controllers/orderController.js";
+import { createOrder,getUserOrders,updatePaymentStatus } from "../controllers/orderController.js";
 import { protect } from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.post("/", protect, createOrder);
 router.get("/", protect, getUserOrders);
+router.patch("/payment-status", protect, updatePaymentStatus);
 export default router;
