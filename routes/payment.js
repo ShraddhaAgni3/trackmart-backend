3a16db9bf92281d0908cf966b5af7c0793bdb20d
ShraddhaAgni3 <155717879+ShraddhaAgni3@users.noreply.github.com>
2026-04-04 15:31:21 +0530

    Update payment.js

--- routes/payment.js
+++ routes/payment.js
@@ -1,22 +1,9 @@
 import express from "express";
-import {
-  createOrder,
-  verifyPayment,
-  createVendorOrder,
-  verifyVendorPayment
-} from "../controllers/paymentController.js";
+import { createOrder, verifyPayment } from "../controllers/paymentController.js";
 
 const router = express.Router();
 
-/* ================= CUSTOMER ================= */
-
-router.post("/create-order", createOrder);   // unchanged
-router.post("/verify", verifyPayment);       // unchanged
-
-
-/* ================= VENDOR ================= */
-
-router.post("/create-vendor-order", createVendorOrder);
-router.post("/verify-vendor", verifyVendorPayment);
+router.post("/create-order", createOrder);
+router.post("/verify", verifyPayment);
 
 export default router;
