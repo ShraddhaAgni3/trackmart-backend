146e7e63fd0225f1986aa0d4ead045a638163605
ShraddhaAgni3 <155717879+ShraddhaAgni3@users.noreply.github.com>
2026-04-09 20:26:59 +0530

    Update sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,11 +3,14 @@ import nodemailer from "nodemailer";
 export const sendEmail = async ({ to, subject, text }) => {
 
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // important
     auth: {
       user: process.env.EMAIL,
       pass: process.env.PASSWORD,
     },
+    connectionTimeout: 10000, // 10 sec
   });
 
   await transporter.sendMail({
@@ -16,5 +19,4 @@ export const sendEmail = async ({ to, subject, text }) => {
     subject,
     text,
   });
-
 };
