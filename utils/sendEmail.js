2b3a94affe74d01c988987392962790e1943fe16
ShraddhaAgni3 <155717879+ShraddhaAgni3@users.noreply.github.com>
2026-04-09 20:38:41 +0530

    Update sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -5,12 +5,12 @@ export const sendEmail = async ({ to, subject, text }) => {
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
-    secure: false, // important
+    secure: false,
     auth: {
       user: process.env.EMAIL,
       pass: process.env.PASSWORD,
     },
-    connectionTimeout: 10000, // 10 sec
+    connectionTimeout: 10000,
   });
 
   await transporter.sendMail({
