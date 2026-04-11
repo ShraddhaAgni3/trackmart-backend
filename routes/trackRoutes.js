14a7f00e17c507f9fc4116c58551e9f4b96ebc0c
ShraddhaAgni3 <155717879+ShraddhaAgni3@users.noreply.github.com>
2026-04-11 20:39:15 +0530

    Update trackRoutes.js

--- routes/trackRoutes.js
+++ routes/trackRoutes.js
@@ -4,10 +4,9 @@ import { protect } from "../middleware/auth.js";
 
 const router = express.Router();
 
+router.patch("/update-location", updateLocation);
 // 🔥 COMMON ROUTE FOR ALL
 router.get("/:itemId", protect, getItemTracking);
 
-
-router.patch("/update-location", updateLocation);
 
 export default router;
