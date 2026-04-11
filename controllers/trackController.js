5dbdfac70a91a6b8351f7d1d7cfb73133e6e5a08
ShraddhaAgni3 <155717879+ShraddhaAgni3@users.noreply.github.com>
2026-04-11 21:48:43 +0530

    Update trackController.js

--- controllers/trackController.js
+++ controllers/trackController.js
@@ -17,8 +17,7 @@ export const getItemTracking = async (req, res) => {
         oi.id AS item_id,
         oi.item_status,
         oi.delivery_date,
-        oi.latitude,
-        oi.longitude,
+        
         oi.vendor_id,
 
         o.user_id,
